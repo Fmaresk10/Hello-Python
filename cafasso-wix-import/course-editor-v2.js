fecd50d13ec0d849d91d5bd3308dc85d11bebc92
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-11 18:45:50 -0300

    Actualizar cargador del editor de cursos

--- cafasso-wix-import/course-editor-v2.js
+++ cafasso-wix-import/course-editor-v2.js
@@ -287,6 +287,6 @@
   console.info('CAFASSO',VERSION);
 
   if(!document.getElementById('cafassoCourseEditorV3Loader')){
-    const s=document.createElement('script');s.id='cafassoCourseEditorV3Loader';s.src='./course-editor-v3.js?v=20260909-1';s.defer=true;document.body.appendChild(s);
+    const s=document.createElement('script');s.id='cafassoCourseEditorV3Loader';s.src='./course-editor-v3.js?v=20260911-2';s.defer=true;document.body.appendChild(s);
   }
 })();
