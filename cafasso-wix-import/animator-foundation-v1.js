96e351c56204cbe60ab825f1d3b5c2742426deaf
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-14 12:28:33 -0300

    Corregir regreso del Patio a la Casa

--- cafasso-wix-import/animator-foundation-v1.js
+++ cafasso-wix-import/animator-foundation-v1.js
@@ -4,7 +4,7 @@
   document.body.className = 'cafasso-foundation';
   const patio = new URLSearchParams(location.search).get('space') === 'patio';
   if (patio) {
-    app.innerHTML = '<main class="cafasso-patio"><img class="cafasso-patio__image" src="https://static.wixstatic.com/media/47bf07_2465a68b3ac64824b43bc20531ce6fd4~mv2.png" alt="Patio salesiano CAFASSO"><button class="cafasso-space-link cafasso-space-link--patio" data-space="patio" type="button">Ir al Patio</button></main>';
+    app.innerHTML = '<main class="cafasso-patio"><img class="cafasso-patio__image" src="https://static.wixstatic.com/media/47bf07_2465a68b3ac64824b43bc20531ce6fd4~mv2.png" alt="Patio salesiano CAFASSO"><button class="cafasso-space-link cafasso-space-link--patio" data-space="house" type="button">Casa</button></main>';
   } else {
     app.innerHTML = '<main class="cafasso-house"><img class="cafasso-house__image" src="./assets/cafasso-casa-interior-v2.jpg" alt="Interior cálido de la Casa CAFASSO"><button class="cafasso-space-link cafasso-space-link--casa" data-space="patio" type="button">Patio</button></main>';
   }
