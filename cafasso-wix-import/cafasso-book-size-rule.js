8bb1a68a98da6e9c208fc84796804e3b847dd350
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-14 23:11:08 -0300

    Cargar la vela de oración en Parroquia

--- cafasso-wix-import/cafasso-book-size-rule.js
+++ cafasso-wix-import/cafasso-book-size-rule.js
@@ -52,6 +52,7 @@
     loadHouseScript('./patio-experience.js?v=1', 'cafasso-patio-experience-loader');
     loadHouseScript('./parish-experience.js?v=1', 'cafasso-parish-experience-loader');
     loadHouseScript('./parish-songbook.js?v=1', 'cafasso-parish-songbook-loader');
+    loadHouseScript('./parish-candle.js?v=1', 'cafasso-parish-candle-loader');
   }
 
   if (document.readyState === 'loading') {
