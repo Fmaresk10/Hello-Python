6cda923e3f05f77001e495d2e3dd86a0c127c425
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-15 10:02:01 -0300

    Actualizar pantalla legible de Escuela

--- cafasso-wix-import/cafasso-book-size-rule.js
+++ cafasso-wix-import/cafasso-book-size-rule.js
@@ -54,7 +54,7 @@
     loadHouseScript('./house-microinteractions.js?v=1', 'cafasso-house-micro-loader');
     loadHouseScript('./patio-experience.js?v=1', 'cafasso-patio-experience-loader');
     loadHouseScript('./school-experience.js?v=1', 'cafasso-school-experience-loader');
-    loadHouseScript('./school-screen-integration.js?v=1', 'cafasso-school-screen-integration-loader');
+    loadHouseScript('./school-screen-integration.js?v=2', 'cafasso-school-screen-integration-loader');
 
     // Primero fijamos la geometría final. Así F5 no muestra posiciones antiguas.
     loadHouseScript('./parish-lectionary-position.js?v=2', 'cafasso-parish-lectionary-position-loader');
