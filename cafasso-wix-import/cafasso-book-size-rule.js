352c9519f5119bf0b77853af00bb7e8061a680ce
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-16 08:40:18 -0300

    Cargar cuarto secreto oculto de Parroquia

--- cafasso-wix-import/cafasso-book-size-rule.js
+++ cafasso-wix-import/cafasso-book-size-rule.js
@@ -81,6 +81,7 @@
     loadHouseScript('./parish-songbook-interior-v2.js?v=1', 'cafasso-parish-songbook-interior-v2-loader');
     loadHouseScript('./parish-candle.js?v=2', 'cafasso-parish-candle-loader');
     loadHouseScript('./parish-candle-altar.js?v=2', 'cafasso-parish-candle-altar-loader');
+    loadHouseScript('./cafasso-parish-secret.js?v=1', 'cafasso-parish-secret-loader');
   }
 
   if (document.readyState === 'loading') {
