503976e16e33c6e06759e30a8674de5f81f65cb4
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-16 10:25:57 -0300

    Cargar transformación Acompañante en la Casa

--- cafasso-wix-import/cafasso-book-size-rule.js
+++ cafasso-wix-import/cafasso-book-size-rule.js
@@ -58,6 +58,7 @@
     loadHouseScript('./cafasso-huella-rewards-v2.js?v=1', 'cafasso-huella-rewards-v2-loader');
     loadHouseScript('./cafasso-admin-gifts-client-v2.js?v=3', 'cafasso-admin-gifts-client-v2-loader');
     loadHouseScript('./cafasso-levels.js?v=2', 'cafasso-levels-loader');
+    loadHouseScript('./cafasso-acompanante-bitacora.js?v=1', 'cafasso-acompanante-bitacora-loader');
 
     loadHouseScript('./cafasso-world-unlocks.js?v=1', 'cafasso-world-unlocks-loader');
     loadHouseScript('./cafasso-world-unlocks-position.js?v=1', 'cafasso-world-unlocks-position-loader');
