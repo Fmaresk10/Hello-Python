2d6a5b604375f14128f21506d1af436374612eb3
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-16 11:24:18 -0300

    Cargar transformación final Corazón salesiano

--- cafasso-wix-import/cafasso-book-size-rule.js
+++ cafasso-wix-import/cafasso-book-size-rule.js
@@ -58,6 +58,7 @@
     loadHouseScript('./cafasso-huella-rewards-v2.js?v=1', 'cafasso-huella-rewards-v2-loader');
     loadHouseScript('./cafasso-admin-gifts-client-v2.js?v=3', 'cafasso-admin-gifts-client-v2-loader');
     loadHouseScript('./cafasso-levels.js?v=2', 'cafasso-levels-loader');
+    loadHouseScript('./cafasso-corazon-salesiano.js?v=1', 'cafasso-corazon-salesiano-loader');
     loadHouseScript('./cafasso-acompanante-bitacora.js?v=1', 'cafasso-acompanante-bitacora-loader');
     loadHouseScript('./cafasso-acompanante-marker-fix.js?v=1', 'cafasso-acompanante-marker-fix-loader');
 
