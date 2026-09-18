4014418d643aef1aca5d53826b2e7689b7b2f95f
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-18 20:05:18 -0300

    Activar Escuela Mobile panorámica

--- cafasso-wix-import/cafasso-book-size-rule.js
+++ cafasso-wix-import/cafasso-book-size-rule.js
@@ -96,6 +96,7 @@
     loadHouseScript('./cafasso-mobile-foundation.js?v=1', 'cafasso-mobile-foundation-loader');
     loadHouseScript('./cafasso-mobile-house.js?v=6', 'cafasso-mobile-house-loader');
     loadHouseScript('./cafasso-mobile-patio.js?v=1', 'cafasso-mobile-patio-loader');
+    loadHouseScript('./cafasso-mobile-school.js?v=1', 'cafasso-mobile-school-loader');
   }
 
   if (document.readyState === 'loading') document.addEventListener('DOMContentLoaded', installRule, { once: true });
