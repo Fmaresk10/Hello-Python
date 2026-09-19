4c2e97ccdc02d8278afcd9c826c92ad5847e0f0a
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-19 19:08:23 -0300

    Cargar nueva Palabra del dia visual

--- cafasso-wix-import/cafasso-book-size-rule.js
+++ cafasso-wix-import/cafasso-book-size-rule.js
@@ -88,6 +88,7 @@
     loadHouseScript('./parish-experience.js?v=1', 'cafasso-parish-experience-loader');
     loadHouseScript('./parish-lectionary-realism.js?v=1', 'cafasso-parish-lectionary-realism-loader');
     loadHouseScript('./parish-lectionary-polish.js?v=3', 'cafasso-parish-lectionary-polish-loader');
+    loadHouseScript('./parish-lectionary-image-v2.js?v=1', 'cafasso-parish-lectionary-image-v2-loader');
     loadHouseScript('./parish-songbook.js?v=2', 'cafasso-parish-songbook-loader');
     loadHouseScript('./parish-songbook-realism.js?v=1', 'cafasso-parish-songbook-realism-loader');
     loadHouseScript('./parish-songbook-interior-v2.js?v=1', 'cafasso-parish-songbook-interior-v2-loader');
