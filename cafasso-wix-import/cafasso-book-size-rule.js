32e53825b6cc14c4b9c2270d75fd53acdf56bdd0
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-19 22:18:45 -0300

    Actualizar sync cloud de Bitácora acompañante

--- cafasso-wix-import/cafasso-book-size-rule.js
+++ cafasso-wix-import/cafasso-book-size-rule.js
@@ -70,7 +70,7 @@
     loadHouseScript('./cafasso-admin-gifts-client-v2.js?v=3', 'cafasso-admin-gifts-client-v2-loader');
     loadHouseScript('./cafasso-levels.js?v=2', 'cafasso-levels-loader');
     loadHouseScript('./cafasso-corazon-salesiano.js?v=1', 'cafasso-corazon-salesiano-loader');
-    loadHouseScript('./cafasso-acompanante-bitacora.js?v=1', 'cafasso-acompanante-bitacora-loader');
+    loadHouseScript('./cafasso-acompanante-bitacora.js?v=2', 'cafasso-acompanante-bitacora-loader');
     loadHouseScript('./cafasso-acompanante-marker-fix.js?v=1', 'cafasso-acompanante-marker-fix-loader');
 
     loadHouseScript('./cafasso-world-unlocks.js?v=1', 'cafasso-world-unlocks-loader');
