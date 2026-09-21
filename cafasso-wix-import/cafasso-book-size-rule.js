e76ebd9af34ff7a9e3bcf8ce916a60cbbe03bdb1
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-21 12:32:35 -0300

    Cargar entrada inmersiva de Escuela

--- cafasso-wix-import/cafasso-book-size-rule.js
+++ cafasso-wix-import/cafasso-book-size-rule.js
@@ -84,6 +84,7 @@
     loadHouseScript('./school-experience.js?v=1', 'cafasso-school-experience-loader');
     loadHouseScript('./cafasso-school-secret.js?v=1', 'cafasso-school-secret-loader');
     loadHouseScript('./school-screen-integration.js?v=2', 'cafasso-school-screen-integration-loader');
+    loadHouseScript('./cafasso-school-immersion.js?v=1', 'cafasso-school-immersion-loader');
     loadHouseScript('./school-resume.js?v=2', 'cafasso-school-resume-loader');
     loadHouseScript('./school-resume-position.js?v=3', 'cafasso-school-resume-position-loader');
 
