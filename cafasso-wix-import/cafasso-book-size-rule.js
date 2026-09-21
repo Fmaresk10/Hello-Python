25a08e8441852a0efd47c744ea35c476548732aa
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-21 11:47:27 -0300

    Actualizar layout de ficha en Casa

--- cafasso-wix-import/cafasso-book-size-rule.js
+++ cafasso-wix-import/cafasso-book-size-rule.js
@@ -105,7 +105,7 @@
     loadHouseScript('./cafasso-mobile-school.js?v=1', 'cafasso-mobile-school-loader');
     loadHouseScript('./cafasso-mobile-parish.js?v=4', 'cafasso-mobile-parish-loader');
     loadHouseScript('./cafasso-mobile-resources.js?v=1', 'cafasso-mobile-resources-loader');
-    loadHouseScript('./cafasso-house-v3-layout.js?v=7', 'cafasso-house-v3-layout-loader');
+    loadHouseScript('./cafasso-house-v3-layout.js?v=8', 'cafasso-house-v3-layout-loader');
     loadHouseScript('./cafasso-patio-v3-layout.js?v=1', 'cafasso-patio-v3-layout-loader');
     loadHouseScript('./cafasso-parish-v3-layout.js?v=12', 'cafasso-parish-v3-layout-loader');
   }
