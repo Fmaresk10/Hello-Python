4fe275ba235b5b501cfcdd525316d5bde0cf9a7d
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-21 12:59:50 -0300

    Publish refined Escuela visuals

--- cafasso-wix-import/cafasso-book-size-rule.js
+++ cafasso-wix-import/cafasso-book-size-rule.js
@@ -83,10 +83,10 @@
     loadHouseScript('./school-course-auth.js?v=1', 'cafasso-school-course-auth-loader');
     loadHouseScript('./school-experience.js?v=1', 'cafasso-school-experience-loader');
     loadHouseScript('./cafasso-school-secret.js?v=1', 'cafasso-school-secret-loader');
-    loadHouseScript('./school-screen-integration.js?v=2', 'cafasso-school-screen-integration-loader');
-    loadHouseScript('./cafasso-school-immersion.js?v=2', 'cafasso-school-immersion-loader');
-    loadHouseScript('./school-resume.js?v=2', 'cafasso-school-resume-loader');
-    loadHouseScript('./school-resume-position.js?v=3', 'cafasso-school-resume-position-loader');
+    loadHouseScript('./school-screen-integration.js?v=3', 'cafasso-school-screen-integration-loader');
+    loadHouseScript('./cafasso-school-immersion.js?v=3', 'cafasso-school-immersion-loader');
+    loadHouseScript('./school-resume.js?v=3', 'cafasso-school-resume-loader');
+    loadHouseScript('./school-resume-position.js?v=4', 'cafasso-school-resume-position-loader');
 
     loadHouseScript('./parish-lectionary-position.js?v=2', 'cafasso-parish-lectionary-position-loader');
     loadHouseScript('./parish-candle-position.js?v=2', 'cafasso-parish-candle-position-loader');
