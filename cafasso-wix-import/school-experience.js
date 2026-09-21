4f08a54d3671ee9f9ff38a7684f444129a332002
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-21 17:34:45 -0300

    Refresh hardened mission player

--- cafasso-wix-import/school-experience.js
+++ cafasso-wix-import/school-experience.js
@@ -318,7 +318,7 @@
           if (!module || !id) return;
           const target = new URL('./course-player.html', location.href);
           target.searchParams.set('player', '1');
-          target.searchParams.set('playerBuild', '5');
+          target.searchParams.set('playerBuild', '6');
           target.searchParams.set('course', String(courseId));
           target.searchParams.set('module', id);
           target.hash = 'modulo';
