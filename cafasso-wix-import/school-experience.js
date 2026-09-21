8ce32ad4348bdfd0ea991d76822bc325ae996201
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-21 17:27:26 -0300

    Refresh mission profile player

--- cafasso-wix-import/school-experience.js
+++ cafasso-wix-import/school-experience.js
@@ -312,7 +312,7 @@
           if (!module || !id) return;
           const target = new URL('./course-player.html', location.href);
           target.searchParams.set('player', '1');
-          target.searchParams.set('playerBuild', '4');
+          target.searchParams.set('playerBuild', '5');
           target.searchParams.set('course', String(courseId));
           target.searchParams.set('module', id);
           target.hash = 'modulo';
