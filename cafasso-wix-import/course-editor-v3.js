cf0fcb5d20ea349ea56b43da66f1565cb62d0dae
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-21 22:51:06 -0300

    Load evaluation builder

--- cafasso-wix-import/course-editor-v3.js
+++ cafasso-wix-import/course-editor-v3.js
@@ -119,6 +119,9 @@
   if(!document.querySelector('script[data-cafasso-typed-blocks]')){
     const s=document.createElement('script');s.src='./course-block-editor.js?v=20260921-2';s.defer=true;s.dataset.cafassoTypedBlocks='1';document.body.appendChild(s);
   }
+  if(!document.querySelector('script[data-cafasso-evaluation-editor]')){
+    const s=document.createElement('script');s.src='./course-evaluation-editor.js?v=20260921-1';s.defer=true;s.dataset.cafassoEvaluationEditor='1';document.body.appendChild(s);
+  }
   if(!document.querySelector('script[data-cafasso-block-transfer]')){
     const s=document.createElement('script');s.src='./course-block-transfer.js?v=20260904-1';s.defer=true;s.dataset.cafassoBlockTransfer='1';document.body.appendChild(s);
   }
