0955cf7720ca699b3876b19568ddec74b2662c4d
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-21 22:53:04 -0300

    Refresh evaluation editor assets

--- cafasso-wix-import/course-editor-v3.js
+++ cafasso-wix-import/course-editor-v3.js
@@ -117,10 +117,10 @@
     const s=document.createElement('script');s.src='./course-editor-status.js?v=20260904-1';s.defer=true;s.dataset.cafassoStatusGuide='1';document.body.appendChild(s);
   }
   if(!document.querySelector('script[data-cafasso-typed-blocks]')){
-    const s=document.createElement('script');s.src='./course-block-editor.js?v=20260921-2';s.defer=true;s.dataset.cafassoTypedBlocks='1';document.body.appendChild(s);
+    const s=document.createElement('script');s.src='./course-block-editor.js?v=20260921-3';s.defer=true;s.dataset.cafassoTypedBlocks='1';document.body.appendChild(s);
   }
   if(!document.querySelector('script[data-cafasso-evaluation-editor]')){
-    const s=document.createElement('script');s.src='./course-evaluation-editor.js?v=20260921-1';s.defer=true;s.dataset.cafassoEvaluationEditor='1';document.body.appendChild(s);
+    const s=document.createElement('script');s.src='./course-evaluation-editor.js?v=20260921-2';s.defer=true;s.dataset.cafassoEvaluationEditor='1';document.body.appendChild(s);
   }
   if(!document.querySelector('script[data-cafasso-block-transfer]')){
     const s=document.createElement('script');s.src='./course-block-transfer.js?v=20260904-1';s.defer=true;s.dataset.cafassoBlockTransfer='1';document.body.appendChild(s);
