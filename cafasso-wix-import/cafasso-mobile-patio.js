dbdd7723f086a14996468e8fdc465e74afef2842
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-22 12:15:03 -0300

    Keep mobile panorama off desktop

--- cafasso-wix-import/cafasso-mobile-patio.js
+++ cafasso-wix-import/cafasso-mobile-patio.js
@@ -1,6 +1,7 @@
 (() => {
   const params = new URLSearchParams(location.search);
   if (params.get('space') !== 'patio') return;
+  if (!window.matchMedia?.('(max-width: 820px), (pointer: coarse)').matches) return;
   if (window.__cafassoMobilePatioInstalled) return;
   window.__cafassoMobilePatioInstalled = true;
 
