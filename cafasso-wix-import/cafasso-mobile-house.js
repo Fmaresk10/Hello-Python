9268189c6879472f3caf402b34efe1a2956bfd1c
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-23 15:24:15 -0300

    Lower Admin monitor on mobile Casa

--- cafasso-wix-import/cafasso-mobile-house.js
+++ cafasso-wix-import/cafasso-mobile-house.js
@@ -124,7 +124,7 @@
 
       html.cafasso-mobile.cafasso-mobile-portrait .cafasso-house-panorama .cafasso-admin-monitor{
         left:69.2%!important;
-        top:56.7%!important;
+        top:61.2%!important;
         width:clamp(112px,17.3vh,146px)!important;
         height:clamp(99px,15.3vh,129px)!important;
         transform:perspective(900px) rotateX(3.5deg) rotateY(-6deg) rotateZ(.35deg)!important;
