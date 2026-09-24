b32af8ebc294cb75d17412ea527cf8c44e83ee67
Fmaresk10 <132764647+Fmaresk10@users.noreply.github.com>
2026-09-24 11:15:36 -0300

    Refresh PWA after module resolver fix

--- cafasso-wix-import/service-worker.js
+++ cafasso-wix-import/service-worker.js
@@ -1,4 +1,4 @@
-const CAFASSO_CACHE='cafasso-shell-20260924-9';
+const CAFASSO_CACHE='cafasso-shell-20260924-10';
 const SCOPE=self.registration.scope;
 const url=path=>new URL(path,SCOPE).href;
 const OFFLINE=url('./offline.html');
